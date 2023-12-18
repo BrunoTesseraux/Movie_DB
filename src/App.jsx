6d81837e60ac8a93621718621b53d180bd1f3b25
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2023-12-18 19:21:56 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { MovieContextProvider } from "./components/Context/MovieContext";
 import Header from "./components/Header/Header";
 import Home from "./page/Home";
 import Details from "./components/Details/Details";
+import MovieList from "./components/MovieList/MovieList";
 import FetchMovies from "./components/FetchData/FetchMovies";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/detail/:id" element={<Details />} />
+          <Route path="/movies" element={<MovieList />} />
         </Routes>
         {/* <Footer /> */}
       </MovieContextProvider>
