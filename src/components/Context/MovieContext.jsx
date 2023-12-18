9b8e92252f09338a7dc103f580b11b07b18809d2
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2023-12-18 17:35:08 +0100

    change state position

--- src/components/Context/MovieContext.jsx
+++ src/components/Context/MovieContext.jsx
@@ -3,7 +3,17 @@ import { createContext, useState } from "react";
 const MovieContext = createContext([]);
 
 const MovieContextProvider = ({ children }) => {
-  return <MovieContext.Provider value={{}}>{children}</MovieContext.Provider>;
+  const [allMovies, setAllMovies] = useState([]);
+  const [config, setConfig] = useState([]);
+  const [genres, setGenres] = useState([]);
+
+  return (
+    <MovieContext.Provider
+      value={{ allMovies, setAllMovies, config, setConfig, genres, setGenres }}
+    >
+      {children}
+    </MovieContext.Provider>
+  );
 };
 
 export { MovieContext, MovieContextProvider };
