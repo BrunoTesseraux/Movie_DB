9a361d130862e94689ebbe940b277b99582b1260
johann3r <johannerrombach@gmail.comm>
2023-12-19 09:06:41 +0100

    Detail page Aufbau

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,14 @@
 import "./Button.scss";
-const Button = () => {
-  return <></>;
+
+const Button = (props) => {
+  return (
+    <>
+      <button className="primary-btn-icon ">
+        <img src={props.icon} alt="" />
+        {props.content}
+      </button>
+    </>
+  );
 };
 
 export default Button;
--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,51 +1,81 @@
 import { useParams } from "react-router-dom";
-
 import "./Details.scss";
-const Details = () => {
-  const selectedMovie = useParams();
-  console.log(selectedMovie);
+import { useContext, useState } from "react";
+import { MovieContext } from "../Context/MovieContext";
+import Button from "../Button/Button";
+import play from "./../../assets/icons/play.svg";
+import rating from "./../../assets/icons/rating.svg";
 
-  const selectedMovieID = selectedMovie.id;
+const Details = () => {
+  const { allMovies } = useContext(MovieContext);
+  console.log(allMovies);
+
+  const { config } = useContext(MovieContext);
+  console.log(config);
+
+  const { fetchGenre } = useContext(MovieContext);
+  console.log(fetchGenre);
+
+  const selectedMoviePath = useParams();
+  console.log(selectedMoviePath);
+
+  const selectedMovieID = selectedMoviePath.id;
   console.log(selectedMovieID);
+
+  const selectedMovieInfos = allMovies.filter((movie) => {
+    return movie.id.toString() === selectedMovieID.toString();
+  });
+  console.log(selectedMovieInfos);
+
+  const [video, setVideo] = useState();
 
   return (
     <>
       <section>
-        <article className="movie-details">
-          <div className="movie-details-top">
-            <h1>Movie Name</h1>
-            <div className="features">
-              <button>back</button>
-              <button>save</button>
-              <button>download</button>
+        {selectedMovieInfos.map((props) => (
+          <article className="movie-details" key={props.id}>
+            <div className="movie-details-top">
+              {/* import poster?? */}
+              <img
+                src={`${config.base_url}.${props.poster_path}`}
+                alt=""
+                className="poster"
+              />
+              <h1>{props.title}</h1>
+              <div className="features">
+                <button>back</button>
+                <button>save</button>
+                <button>download</button>
+              </div>
+              <div className="key-infos">
+                <p>
+                  <img src={rating} alt="" /> {props.vote_average}
+                </p>
+                <p>{props.release_date}</p>
+                <p>Genre 1</p>
+                <p>props.runtime</p>
+              </div>
             </div>
-            <div className="key-infos">
-              <p>8.0</p>
-              <p>Erschienen</p>
-              <p>Genre 1</p>
-              <p>Länge</p>
+            <div className="movie-details-bottom">
+              <h2>Overview</h2>
+              <div>
+                <p className="overview-text">{props.overview}</p>
+                <button className="inline-btn">See more</button>
+              </div>
+              <div className="genres">
+                <p>Genre1</p>
+                <p>Genre2</p>
+                <p>Genre3</p>
+              </div>
+              <div className="languages">
+                <p>{props.original_language}</p>
+                <p>language2</p>
+                <p>language3</p>
+              </div>
+              <Button icon={play} content="Watch now"></Button>
             </div>
-          </div>
-          <div className="movie-details-bottom">
-            <h2>Overview</h2>
-            <p>
-              {" "}
-              Beschreibung, Lorem, ipsum dolor sit amet consectetur adipisicing
-              elit. Magni, ad!
-            </p>
-            <div className="genres">
-              <p>Genre1</p>
-              <p>Genre2</p>
-              <p>Genre3</p>
-            </div>
-            <div className="languages">
-              <p>language1</p>
-              <p>language2</p>
-              <p>language3</p>
-            </div>
-            <button>Watch trailer</button>
-          </div>
-        </article>
+          </article>
+        ))}
       </section>
     </>
   );
